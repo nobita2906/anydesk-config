39f45c195f3534e4647a5aa63199f3a454c5fa67
nobita2906 <140187170+nobita2906@users.noreply.github.com>
2023-09-26 17:56:40 +0700

    Update setupAnydesk.bat

--- setupAnydesk.bat
+++ setupAnydesk.bat
@@ -1,6 +1,6 @@
 :: Tải AnyDesk từ GitHub
 certutil -urlcache -split -f "https://github.com/falconx1/windows-config/raw/main/AnyDesk.exe" "C:\Users\Public\Downloads\AnyDesk.exe"
 cd /d C:\Users\Public\Downloads\
-start /wait anydesk.exe --install "C:\Program Files (x86)\AnyDesk" --start-with-win --create-desktop-icon --set "ProxyPolicy=never"
+start /wait anydesk.exe --install "C:\Program Files (x86)\AnyDesk" --start-with-win --create-desktop-icon
 cd /d "C:\Program Files (x86)\AnyDesk
-echo Global@13 | anydesk.exe --set-password
+echo Global@13 | anydesk.exe --set-password --set "ProxyPolicy=never"
